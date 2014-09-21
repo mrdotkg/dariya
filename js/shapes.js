a4fc8086320af7d5f043f955d57c549d58d12966
Kumar Gaurav <grv.rkg@gmail.com>
2014-09-22 02:08:42 +0530

    all granths

--- js/shapes.js
+++ js/shapes.js
@@ -320,8 +320,7 @@ $(document).ready(function() {
                             $('#preview-loader').removeClass('show');
                             $(window).trigger('resize');
                             var a=document.getElementsByClassName('pdf flex-active-slide');
-//                            a[0].innerHTML='<iframe src="'+elem.find('.project-media').data('granth')+'" height = "100%" frameborder="no" width = "100%"></iframe>';
-                            a[0].innerHTML=' <iframe src="http://docs.google.com/gview?url=http://measdot.github.io/dariya/assets/pdf/viveksagar.pdf&embedded=true" style="width:790px; height:500px;" frameborder="0">Loading...</iframe>';
+                            a[0].innerHTML=' <iframe src="http://docs.google.com/gview?url=http://measdot.github.io/dariya/'+elem.find('.project-media').data('granth')+'&embedded=true" style="width:790px; height:500px;" frameborder="0">Loading...</iframe>';
                         },1000)
                     }
                 });
